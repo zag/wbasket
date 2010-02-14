493590bc4b1023a95b1ee7b7ea7352fdace9fda4
zag <zag@d2c74b2b-9965-db11-a26f-001731543224>
2010-02-14 10:37:24 +0000

    extra lib
    git-svn-id: file:///home/zag/svn/Alert2@714 d2c74b2b-9965-db11-a26f-001731543224

--- extra/wbadget.js
+++ extra/wbadget.js
@@ -17,6 +17,9 @@ if (window.wimho === undefined) {
     if (typeof JS_API_PATH == "undefined" ) {
         JS_API_PATH=JS_HOST+'/distrib_api'
     }
+    if ( typeof imhonet_r == "undefined") {
+    imhonet_r = Math.round(Math.random() * 100000);
+    }
 
 }
 if (window.wimho.commonUrl === undefined) {
@@ -49,7 +52,7 @@ if (! __WIMHO_JS_INCLUDED) {
         this.warn = function( message) {this.message(message,2) };
         this.info = function( message) {this.message(message,2) };
 
-        if( inlevel ) {
+        if( typeof console != "undefined" && inlevel ) {
             this.loglevel = inlevel
         }
     };
@@ -94,7 +97,7 @@ if (! __WIMHO_JS_INCLUDED) {
     };
 
 //    GWanted.__GWANTED_JS_LOAD_START_TIME = new LogTimer(true);
-    wimho.log =new LogFirebug( 2);
+    wimho.log = new LogFirebug( 2 );
 //    GWanted.Version = '0.0.4';
     
     wimho.loadedModules  = [];
@@ -189,6 +192,12 @@ if (! __WIMHO_JS_INCLUDED) {
 	// Return the modified object
 	return target;
     };
+	// This may seem like some crazy code, but trust me when I say that this
+	// is the only cross-browser way to do this. --John
+    wimho.isFunction=function( fn ) {
+		return !!fn && typeof fn != "string" && !fn.nodeName && 
+			fn.constructor != Array && /function/i.test( fn + "" );
+	};
 
     wimho.$ = function () {
         return '+';
@@ -237,7 +246,6 @@ if (! __WIMHO_JS_INCLUDED) {
         var num=0;
         for (i = 0; i < pr.length ; i++)  {
             var str = pr[i][0] ;
-            //if ( str && typeof str == "number" )  { actual_scale.push(pr[i])}
             if ( str && ( typeof str == "number" || str.match(/[-\d]+/) ) )   { actual_scale.push(pr[i])}
         }
         return actual_scale;
@@ -253,6 +261,21 @@ if (! __WIMHO_JS_INCLUDED) {
         var text =  pr[idx][1];
         return { 'value': value, 'text': text };
     };
+
+    wimho.rate2idx = function ( value) {
+        var pr = wimho.actual_rate_scale(wimho.get_defs().rate_custom);
+        var idx = 0;
+        for (i = 0; i < pr.length ; i++) {
+            var rvalue = pr[i][0];
+            if ( value == rvalue) {
+                idx = i+1;
+                break;
+            }
+        }
+        wimho.log.info("return idx:"+ idx+ " for rate " + value );
+        return idx;
+    };
+
 
     /* Run */
     (function(){
@@ -432,6 +455,68 @@ if (! __WIMHO_JS_INCLUDED) {
         }
     };
 
+wimho.extend(
+    wimho, {
+    get_rate_init_url : function (attr, params) {
+    //"http://connector.imhonet.ru/distrib_api/50bbeadaa17f02e1fdf9f216af1e79b6/1210753367/37427125/1410/w_rate.json?on_get_rate=1&keywords=85174";
+        attr.method="w_rate.json";
+        var pars = this.get_defs();
+        if (! params) {
+            params = {};}
+        if (  pars.encode != 'utf8' ) {
+            params.encode = pars.encode;}
+        if ( ! pars.rate_custom_p ) {
+            params.nodefs_rate = 1;}
+       if (attr.on_get_rate && wimho.isFunction(attr.on_get_rate))  {
+	wimho.extend(params,{"on_get_rate": 1});}
+	    
+	params.keywords = imhonet_r;
+        var url = this.get_req_url(attr, params);
+        return url;
+
+    },
+        get_post_url : function ( attr , value ) {
+        attr.method="edit_rate.json";
+	
+	var params = {};
+	params.keywords = imhonet_r;
+	params.value = value;
+	if (attr["content"]) { 
+	    params.toid = attr["content"];}
+        var url = this.get_req_url( attr , params);
+        this.log.warn("POST URL URL"+ url);
+        return url;
+    },
+
+        get_req_url : function (attr,params) {
+	var reg = /[\s,\?,\/]+/g;
+	var defaults = {soid: 0, ssid:0, sign:0, cid:0 };
+	for (var k in defaults) {
+	    if (attr[k]) {
+		if(reg.test(attr[k])) {
+		    params[k] = attr[k];
+		}else{
+		    defaults[k] = attr[k];
+		}
+	    } 
+	}
+        //$_.extend ( defaults, attr);
+        // fill extra params to url
+        var s = [];
+        // Serialize the key/values
+	for ( var j in params ) {
+	    if ( !(j == "value" && params[j] == "_none") )
+	    	s.push( encodeURIComponent(j) + "=" + encodeURIComponent( params[j] ) );
+	}
+        //var cgi_pars = s.join("&").replace(/%20/g, "+");
+        var cgi_pars = s.join("&");
+        var url = [JS_API_PATH,defaults.sign, defaults.cid,defaults.ssid, defaults.soid, attr.method ].join("/");
+        if (cgi_pars) 
+            url +='?'+cgi_pars;
+        return url;
+    }
+
+    });
     function imhonet_rate_print (params, container) {
         wimho.log.info("b:imhonet_rate_print");
         wimho.makeWidgetNest('Popup', params, container);
